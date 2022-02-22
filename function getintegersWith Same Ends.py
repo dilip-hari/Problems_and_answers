ce679c2efb24ac75479ae6122f7036172acb906c
dilip0207 <54951927+dilip0207@users.noreply.github.com>
2022-02-23 00:02:19 +0530

    Update function getintegersWith Same Ends.py

--- function getintegersWith Same Ends.py
+++ function getintegersWith Same Ends.py
@@ -1,3 +1,68 @@
+'''function getintegersWith Same Ends
+
+The function/method getIntegersWithSame Ends  . accepts two arguments str and K. The first argument str
+represents a string value containing only digits. The second argument K represents an integer value.
+
+The function/method getintegersWithSame Ends must form all possible K-digit integers with the
+same must for ends using the consecutive digits in the given string. Then the program must return an
+array of integers  containing the resulting K-digit integers in sorted order. If there is no such
+K-digit integer, then the  function must return an array of size 1 with the value as -1.
+
+Note: K-digit integers do not have leading zeroes.
+
+Your task is to implement the function getIntegersWithSameEnds so that it passes all the test cases.
+
+
+Boundary Condition(s):
+
+1 <= Length of S<= 100
+
+2 <= K <= 9
+
+Example Input/Output 1:
+
+Input:
+
+508562102042022
+
+4
+
+Output:
+
+2022 2042 2102 5085
+
+Explanation:
+
+Here K = 4.
+
+The 4-digit integers with the same ends that can be formed from the string are given below.
+
+5085, 2102, 2042 and 2022
+
+After sorting the 4-digit integers, the integers become
+
+2022 2042 2102 5085
+
+Example Input/Output 2:
+
+Input:
+
+909086733834444
+
+3
+
+Output:
+
+383 444 444 909
+
+Example Input/Output 3:
+Input:
+
+909086733834
+
+Output:
+
+-1'''
 def integer_with_same_ends(n,k):
     i=0
     a=[]
