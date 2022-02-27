9e57c3b37ad898204a16a613be2d3feb9bb3cbb5
dilip0207 <54951927+dilip0207@users.noreply.github.com>
2022-02-27 23:59:57 +0530

    Update Sort the given list according to frequency of elements.py

--- Sort the given list according to frequency of elements.py
+++ Sort the given list according to frequency of elements.py
@@ -1,3 +1,22 @@
+'''
+Write a program to sort the given list according to frequency of elements.
+
+Examples input1:
+
+input:[2,3,2,4,5,12,2,3,3,3,12]
+
+output:[3,3,3,3,2,2,2,12,12,4,5]
+
+Example input2:
+
+input:[0,2,1,-1,1,2,0,4,-1,4]
+
+output:[-1,-1,0,0,1,1,2,2,4,4]
+
+Example input3:
+
+input:[4,6,8,2,5,7,9]
+'''
 def frequency(n):
     a=[]
     for i in range(n):
